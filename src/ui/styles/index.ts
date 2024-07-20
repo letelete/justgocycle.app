f09124ef0edb3bd8266f05e094951a56b65ac666
letelete <brunokawka@gmail.com>
2024-07-20 13:20:33 +0200

    Add initial project configuration

---
+++ src/ui/styles/index.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+import tailwindConfig from '../../../tailwind.config';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function vhToPx(vh: number) {
+  return document.documentElement.clientHeight * (vh / 100);
+}
+
+export const tw = resolveConfig(tailwindConfig);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        body: {
+          DEFAULT: 'var(--c-body)',
+          fg: {
+            solid: 'var(--c-body-fg-solid)',
+            primary: 'var(--c-body-fg-primary)',
+            secondary: 'var(--c-body-fg-secondary)',
+            disabled: 'var(--c-body-fg-disabled)',
+            destructive: 'var(--c-body-fg-destructive)',
+          },
+        },
+        accent: {
+          DEFAULT: 'var(--c-accent)',
+          fg: {
+            solid: 'var(--c-accent-fg-solid)',
+          },
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
